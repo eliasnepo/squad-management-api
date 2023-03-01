9deeed800370b4da55db5598020659afb388d3d5
Elias Nepomuceno <eliasnepo12@gmail.com>
2023-03-01 01:49:17 -0300

    chore: add seed data locally

---
+++ src/main/java/com/squad/management/SeedDataLocally.java
@@ -0,0 +1,47 @@
+package com.squad.management;
+
+import com.squad.management.teams.Team;
+import com.squad.management.teams.TeamRepository;
+import com.squad.management.users.User;
+import com.squad.management.users.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Stream;
+
+@Component
+@ConditionalOnProperty(name = "spring.profiles.active", havingValue = "local")
+public class SeedDataLocally {
+
+    private final UserRepository userRepository;
+    private final TeamRepository teamRepository;
+
+    public SeedDataLocally(UserRepository userRepository, TeamRepository teamRepository) {
+        this.userRepository = userRepository;
+        this.teamRepository = teamRepository;
+    }
+
+    @PostConstruct
+    private void init() {
+        /* Seed Data for users */
+        Stream.of(
+                new User(null, "John", "Doe", "jdoe", "https://github.com/profile.png", "United States"),
+                new User(null, "Maria", "Souza", "msouza", "https://github.com/profile.png", "Brazil"),
+                new User(null, "Elias", "Souza", "esouza", "https://github.com/profile.png", "Brazil"),
+                new User(null, "Joey", "Tribbiani", "jtrib", "https://github.com/profile.png", "United States"),
+                new User(null, "Ross", "Geller", "rgeller", "https://github.com/profile.png", "United States"),
+                new User(null, "Monica", "Geller", "mgeller", "https://github.com/profile.png", "United States"),
+                new User(null, "Rachel", "Green", "rgreen", "https://github.com/profile.png", "United States")
+        ).forEach(userRepository::save);
+
+        /* Seed Data for teams */
+        Stream.of(
+                new Team(null, "Product A", null, null),
+                new Team(null, "Product B", null, null),
+                new Team(null, "Product C", null, null),
+                new Team(null, "Product D", null, null),
+                new Team(null, "Product E", null, null)
+        ).forEach(teamRepository::save);
+    }
+}
